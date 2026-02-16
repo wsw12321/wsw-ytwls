db2db145858265d49c570578cf9ed0b88ab167e8
wsw12321 <2183220676@qq.com>
2026-02-16 19:24:30 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
+  base: "/", // Cloudflare Pages 通常使用根路径，或者 "./" 也可以
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: 'dist', // 显式声明输出目录为 dist，虽然这是默认值
+  }
 });
